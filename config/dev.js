13a0d4136e1b363e7065a5b44dcbdca4227d0100
Eunsoo Jung <hi.michael.jung@gmail.com>
2018-05-22 19:23:52 -0700

    Update google client ID

--- config/dev.js
+++ config/dev.js
@@ -4,12 +4,19 @@
  */
 
 module.exports = {
+    googleClientID: '547403104179-rai5kch2q1faupika73ddpsict5f1u8b.apps.googleusercontent.com',
+    googleClientSecret: '3uUOeILQrxnm07AKHQgUXeTD',
+    mongoURI: 'mongodb://michael:1234@ds225010.mlab.com:25010/lpc-profiles',
+    cookieKey: 'dagdkjgdkjaglkddagdagadgadgadgadgadgsadgadg',
+    redirectDomain: 'http://localhost:3000'
+};
+
+/*
+module.exports = {
     googleClientID: '547403104179-rlg3l7dcqgjbjukun227pqdndk2f5fhc.apps.googleusercontent.com',
     googleClientSecret: 'aiWQDioaMmhTh_zlAI1R2zaM',
     mongoURI: 'mongodb://michael:1234@ds225010.mlab.com:25010/lpc-profiles',
     cookieKey: 'dagdkjgdkjaglkddagdagadgadgadgadgadgsadgadg',
-    stripePublishableKey: 'pk_test_MGjc0rNMhB8vo2rJjfvJfy1J',
-    stripeSecretKey: 'sk_test_PMXqoOLhYPTqEdZHSXWLcirg',
-    sendGridKey: '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',
     redirectDomain: 'http://localhost:3000'
 };
+ */
